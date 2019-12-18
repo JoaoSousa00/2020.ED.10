ad49b5698187e63fc797f200035d15e9fd9d1c8e
JOAO PEDRO BRANDAO MOREIRA DE SOUSA <8180175@estg.ipp.pt>
2019-12-18 20:34:24 +0000

    Version 1.3.1

--- src/Tests/Test1.java
+++ src/Tests/Test1.java
@@ -26,7 +26,7 @@ public class Test1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n\tTestes Disponiveis:\nTeste 1 - Exercicio 1(ArrayHeap)\nTeste 2 - ...\nTeste 3 - ...\n\nQue Teste pretende efetuar:");
+        System.out.println("\n\tTestes Disponiveis:\nTeste 1 - Exercicio 1(ArrayHeap)\nTeste 2 - Exercicio 2 (PriorityQueue)\nTeste 3 - Exercicio 3 (LinkedHeap)\n\nQue Teste pretende efetuar:");
         int a = scanner.nextInt(4); //Aceita só até 3
 
         switch (a) {
@@ -85,16 +85,11 @@ public class Test1 {
                 break;
 
             case 2:
-                System.out.println("\nEstado do Sistema:\nEstado 1 - Coleção Vazia\nEstado 2 - Coleção só com root\nEstado 3 - Coleção não vazia\n\nQue método pretende executar:");
-                int c = scanner.nextInt(4);
+                System.out.println("Sem testes...");
+                int c = scanner.nextInt(0);
                 switch (c) {
                     case 1:
                         break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-
                 }
 
         }
