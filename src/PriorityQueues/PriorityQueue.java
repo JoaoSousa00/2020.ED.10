4c4aa97e27640679b445526c33a1b0842df4f27f
JOAO PEDRO BRANDAO MOREIRA DE SOUSA <8180175@estg.ipp.pt>
2019-12-18 20:21:33 +0000

    Version 1.3

---
+++ src/PriorityQueues/PriorityQueue.java
@@ -0,0 +1,20 @@
+package PriorityQueues;
+
+/**
+ * <h3>
+ * ESTG - Escola Superior de Tecnologia e Gestão<br>
+ * IPP - Instituto Politécnico do Porto<br>
+ * LEI - Licenciatura em Engenharia Informática<br>
+ * PP - Paradigmas de Programação<br>
+ * </h3>
+ * <p>
+ * <strong>Author: </strong><br>
+ * Joao Sousa<br>
+ * <strong>Description: </strong><br>
+ * Class that represents 
+ * </p>
+ */
+
+public class PriorityQueue {
+
+}
